994b174a7216fec42ef045df9700b194f22455da
Arthur Teixeira <153836849+arthurteixeiradev@users.noreply.github.com>
2024-03-01 20:10:04 -0300

    Update ex4.py

--- list/ex4.py
+++ list/ex4.py
@@ -3,13 +3,11 @@ def media(letra, nota1, nota2, nota3):
     if letra == 'A':
         mediaA = (nota1 + nota2 + nota3) / 3
         resultado = round(mediaA, 2)
-        mensagem = print('A média aritmética das notas:', nota1, ',', nota2, ',', nota3, 'é', resultado)
-        return mensagem
+        return f'A média aritmética das notas: {nota1}, {nota2}, {nota3} é {resultado}'
     elif letra == 'P':
         mediaP = ((nota1 * 5) + (nota2 * 3) + (nota3 * 2)) / 10
         resultado = round(mediaP, 2)
-        mensagem = print('A média ponderada das notas:', nota1, ',', nota2, ',', nota3, 'é', resultado)
-        return mensagem
+        return f'A média ponderada das notas: {nota1}, {nota2}, {nota3} é {resultado}'
     else:
         print('Método inexistente!')
         return
@@ -18,4 +16,4 @@ nota1 = float(input('Digite sua primeira nota: '))
 nota2 = float(input('Digite sua segunda nota: '))
 nota3 = float(input('Digite sua terceira nota: '))
 letra = input('Média aritmetica (A)\nMédia ponderada (P)\nDigite o método: ')
-media(letra, nota1, nota2, nota3)
+print(media(letra, nota1, nota2, nota3))
